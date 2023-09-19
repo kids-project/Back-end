9448c74d99686ae132820f5f9a3a88fb1444fe83
magiof <tjddnjs7559@gamil.com>
2023-09-19 23:19:11 +0900

    fix: origin 와일드카드 제거

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: '*' });
+  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
 
   app.use(cookieParser());
 
