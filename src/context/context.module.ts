0d414f3eb809e3b0ba4c5c7d8c6d65aa78b0e03e
magiof <tjddnjs7559@gamil.com>
2023-09-20 22:31:42 +0900

    feat: inventory 추가

--- src/context/context.module.ts
+++ src/context/context.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { FairyModule } from './fairy/fairy.module';
+import { InventoryModule } from './inventory/inventory.module';
 
 @Module({
-  imports: [UsersModule, FairyModule],
+  imports: [UsersModule, FairyModule, InventoryModule],
 })
 export class ContextModule {}
