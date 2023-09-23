6c400a6636ca041c633fa4f61e6e71abc3edcdcb
magiof <tjddnjs7559@gamil.com>
2023-09-23 16:54:20 +0900

    fix: cors수정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors({
+    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'http://lookids.bucket.s3-website.kr.object.ncloudstorage.com/',
+    ],
+  });
 
   await app.listen(3000);
 }
